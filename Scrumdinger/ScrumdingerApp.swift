cc6b43194e552f395eccd0d4a0afa7377dfe7da3
Fernanda Geraissate <fernandag@spotify.com>
2022-05-02 18:07:35 +0200

    Use Stacks to Arrange Views

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Fernanda Gadeia Geraissate on 2022-05-02.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
