26fac4dc244fc4949da8fc80e6ddf1e555f61d16
Fernanda Geraissate <fernandag@spotify.com>
2022-05-03 16:42:08 +0200

    Create a Navigation Hierarchy

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -24,7 +24,7 @@ enum Theme: String {
     case tan
     case teal
     case yellow
-
+    
     var accentColor: Color {
         switch self {
         case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
@@ -33,5 +33,8 @@ enum Theme: String {
     }
     var mainColor: Color {
         Color(rawValue)
+    }
+    var name: String {
+        rawValue.capitalized
     }
 }
